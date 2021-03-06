7ebcd76767a842b0ccab736db330736637d969d5
Oxyw <63540237+Oxyw@users.noreply.github.com>
2021-03-06 14:02:05 +0800

    Add files via upload

---
+++ Homework1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            double m, n = 0;
+            double result = 0;
+            string sign;
+            Console.WriteLine("请输入第1个操作数：");
+            m = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第2个操作数：");
+            n = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入操作符：");
+            sign = Console.ReadLine();
+            switch (sign)
+            {
+                case "+":
+                    result = m + n;
+                    break;
+                case "-":
+                    result = m - n;
+                    break;
+                case "*":
+                    result = m * n;
+                    break;
+                case "/":
+                    while(n==0)
+                    {
+                        Console.WriteLine("请输入正确的除数：");
+                        n = double.Parse(Console.ReadLine());
+                    }
+                    result = m / n;
+                    break;
+                case "%":
+                    result = m % n;
+                    break;
+                default:
+                    Console.WriteLine("操作符错误！");
+                    return 0;
+            }
+            Console.WriteLine($"{m}{sign}{n}={result}");
+            return 0;
+        }
+    }
+}
---
+++ Homework1/WinFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double m = 0, n = 0;
+            string sign = "";
+            if(comboBox1.SelectedIndex != -1)
+                sign = comboBox1.SelectedItem.ToString();
+            double result = 0;
+            if (textBox2.Text.Trim() == string.Empty || textBox3.Text.Trim() == string.Empty)
+                MessageBox.Show("请输入正确的操作数！");
+            else
+            {
+                m = double.Parse(textBox2.Text);
+                n = double.Parse(textBox3.Text);
+                switch (sign)
+                {
+                    case "+":
+                        result = m + n;
+                        break;
+                    case "-":
+                        result = m - n;
+                        break;
+                    case "*":
+                        result = m * n;
+                        break;
+                    case "/":
+                        if (n == 0)
+                            MessageBox.Show("请输入正确的除数！");
+                        else
+                            result = m / n;
+                        break;
+                    case "%":
+                        result = m % n;
+                        break;
+                    default:
+                        MessageBox.Show("操作符错误！");
+                        break;
+                }
+            }
+            this.textBox1.Text = $"{result}";
+        }
+    }
+}
