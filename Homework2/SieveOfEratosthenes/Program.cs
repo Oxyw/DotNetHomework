82b8485222247109be4a2fb511f13a6b72f14066
Oxyw <63540237+Oxyw@users.noreply.github.com>
2021-03-13 11:49:38 +0800

    Add files via upload

---
+++ Homework2/SieveOfEratosthenes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] a = new bool[100];
+            Array.Clear(a, 0, 100); //数组全部置false
+            a[0] = true; //排除1
+            int i,j;
+            for(i=2; i<=100; i++)
+            {
+                if(a[i-1]==false)
+                {
+                    j = 2*i;
+                    while(j<=100)
+                    {
+                        a[j - 1] = true;
+                        j += i;
+                    }
+                }
+            }
+            for(i = 1; i <= 100; i++)
+            {
+                if(a[i-1]==false)
+                    Console.Write($"{i}\t");
+            }
+        }
+    }
+}
---
+++ Homework2/ToeplitzMatrix/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ToeplitzMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0, n = 0;
+            try
+            {
+                Console.Write("Please enter the number of rows: ");
+                m = int.Parse(Console.ReadLine());
+                Console.Write("Please enter the number of columns: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("解析错误" + e.Message);
+            }
+            int[,] a = new int[m, n];
+            Console.WriteLine("Please enter the matrix: ");
+            int i, j;
+            string stemp;
+            for(i=0; i<m; i++)
+            {
+                stemp = Console.ReadLine();
+                string[] num = Regex.Split(stemp, " ");
+                for (j=0; j<n; j++)
+                {
+                    a[i, j] = int.Parse(num[j]);
+                }
+            }
+            Console.Write(isTplzMatrix(a));
+        }
+
+        public static bool isTplzMatrix(int[,] matrix)
+        {
+            bool flag = true;
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            int i, j, temp;
+            //以[row, 0]为起始斜线扫描
+            i = m - 2;
+            while (flag == true && i > 0)
+            {
+                j = 0;
+                temp = i;
+                while (flag == true && j < n && temp < m)
+                {
+                    if (matrix[temp, j]!=matrix[i,0])
+                        flag = false;
+                    temp++; j++;
+                }
+                i--;
+            }
+            //以[0,col]为起始斜线扫描，i=0
+            j = 0;
+            while (flag == true && j < n)
+            {
+                i = 0;
+                temp = j;
+                while (flag == true && i < m && temp < n)
+                {
+                    if (matrix[i, temp] != matrix[0, j])
+                        flag = false;
+                    temp++; i++;
+                }
+                j++;
+            }
+            return flag;
+        }
+    }
+}
