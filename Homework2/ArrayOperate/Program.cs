34937491eefcb7798825ccfcccd3d9e108886d00
Oxyw <63540237+Oxyw@users.noreply.github.com>
2021-03-13 11:48:34 +0800

    Add files via upload

---
+++ Homework2/ArrayOperate/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ArrayOperate
+{
+    class ArrayOp
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now let's create an array.");
+            Console.Write("Please enter the size of the array:");
+            int n = 0;
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("解析错误" + e.Message);
+            }
+            int[] a = new int[n];
+            InitializeArray(a);
+            Operate1(a);
+        }
+
+        public static void InitializeArray(int[] a)
+        {
+            Console.WriteLine("Please enter some integer: ");
+            for(int i=0; i<a.Length; i++)
+            {
+                try
+                {
+                    a[i] = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("解析错误" + e.Message);
+                }
+            }
+            Console.WriteLine("Finished!");
+        }
+
+        public static void Operate1(int[] a)
+        {
+            int maxNum = a[0], minNum = a[0], sum = 0; ;
+            foreach(int num in a)
+            {
+                sum += num;
+                maxNum = (maxNum > num) ? maxNum : num;
+                minNum = (minNum < num) ? minNum : num;
+            }
+            double average = (double)sum / a.Length;
+            Console.WriteLine($"The max number of the array is: {maxNum}");
+            Console.WriteLine($"The min number of the array is: {minNum}");
+            Console.WriteLine($"The sum of the array is: {sum}");
+            Console.WriteLine($"The average of the array is: {average}");
+        }
+    }
+}
---
+++ Homework2/PrimeFactor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            int num=0;
+            Console.WriteLine("Please enter a number:");
+            try {
+                num = int.Parse(Console.ReadLine());
+            }catch(Exception e)
+            {
+                Console.WriteLine("解析错误" + e.Message);
+            }
+            Console.WriteLine($"The prime factors of {num}:");
+            Operate(num);
+        }
+        public static bool FindPrime(int n)
+        {
+            int i;
+            int j = (int)System.Math.Sqrt(n);
+            bool flag = true;
+            for (i = 2;i<=j;i++)
+            {
+                if(n%i==0)
+                {
+                    flag = false;
+                    return flag;
+                }
+            }
+            return flag;
+        }
+
+        public static void Operate(int n)
+        {
+            int i = 2;
+            while(i<=n)
+            {
+                if(n%i==0 && FindPrime(i)==true)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
