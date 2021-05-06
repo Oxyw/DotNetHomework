5ac8f60914fcb34dd04560ffd8f1775b1e877f1b
Oxyw <63540237+Oxyw@users.noreply.github.com>
2021-05-06 17:54:46 +0800

    Add files via upload

---
+++ Homework9/Crawler/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Crawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.CrawlerInfo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtNum);
+            this.splitContainer1.Panel1.Controls.Add(this.CrawlerInfo);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtUrl);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 96;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtNum.Location = new System.Drawing.Point(673, 26);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(99, 25);
+            this.txtNum.TabIndex = 4;
+            this.txtNum.Text = "10";
+            // 
+            // CrawlerInfo
+            // 
+            this.CrawlerInfo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CrawlerInfo.AutoSize = true;
+            this.CrawlerInfo.Location = new System.Drawing.Point(74, 68);
+            this.CrawlerInfo.Name = "CrawlerInfo";
+            this.CrawlerInfo.Size = new System.Drawing.Size(31, 15);
+            this.CrawlerInfo.TabIndex = 3;
+            this.CrawlerInfo.Text = "   ";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(21, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Info:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtUrl.Location = new System.Drawing.Point(25, 26);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(501, 25);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://www.runoob.com/";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnStart.Location = new System.Drawing.Point(552, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 25);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.URL,
+            this.Status});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Index
+            // 
+            this.Index.DataPropertyName = "Index";
+            this.Index.HeaderText = "Index";
+            this.Index.MinimumWidth = 6;
+            this.Index.Name = "Index";
+            this.Index.Width = 125;
+            // 
+            // URL
+            // 
+            this.URL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.MinimumWidth = 6;
+            this.URL.Name = "URL";
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Crawler";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label CrawlerInfo;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ Homework9/Crawler/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Crawler
+{
+    public partial class Form1 : Form
+    {
+        BindingSource bdsResult = new BindingSource();
+        Crawler crawler = new Crawler();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = bdsResult;
+            crawler.CrawlerDownloaded += PageDownloaded;
+            crawler.CrawlerStopped += Stop;
+            crawler.CrawlerInfo += ShowInfo;
+        }
+
+        private void ShowInfo(Crawler crawler, string url)
+        {
+            Action action = () => CrawlerInfo.Text = "正在爬取" + url;
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void Stop(Crawler obj)
+        {
+            Action action = () => CrawlerInfo.Text = "爬虫已停止";
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void PageDownloaded(Crawler crawler, string url, string info)
+        {
+            var pageInfo = new { Index = bdsResult.Count + 1, URL = url, Status = info };
+            Action action = () => { bdsResult.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bdsResult.Clear();
+            crawler.StartUrl = txtUrl.Text;
+            try
+            {
+                crawler.MaxNum = int.Parse(txtNum.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("请输入正确的整数！");
+            }
+
+            new Thread(crawler.Crawl).Start();   //开始爬行
+            CrawlerInfo.Text = "爬虫已启动....";
+        }
+    }
+}
