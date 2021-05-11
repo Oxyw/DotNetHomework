36048c572af5a76f4d7acbc67de2679d9c4c048d
Oxyw <63540237+Oxyw@users.noreply.github.com>
2021-05-11 21:11:17 +0800

    Add files via upload

---
+++ Homework10/ParallelCrawler/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParallelCrawler
+{
+    public partial class Form1 : Form
+    {
+        BindingSource bdsResult = new BindingSource();
+        ParallelCrawler crawler = new ParallelCrawler();
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = bdsResult;
+            crawler.CrawlerDownloaded += PageDownloaded;
+            crawler.CrawlerStopped += Stop;
+            crawler.CrawlerInfo += ShowInfo;
+        }
+
+        private void ShowInfo(ParallelCrawler crawler, string url)
+        {
+            Action action = () => CrawlerInfo.Text = "正在爬取" + url;
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void Stop(ParallelCrawler obj)
+        {
+            Action action = () => CrawlerInfo.Text = "爬虫已停止";
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void PageDownloaded(ParallelCrawler crawler, int index, string url, string info)
+        {
+            var pageInfo = new { Index = index, URL = url, Status = info };
+            Action action = () => { bdsResult.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bdsResult.Clear();
+            crawler.StartUrl = txtUrl.Text;
+            try
+            {
+                crawler.MaxNum = int.Parse(txtNum.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("请输入正确的整数！");
+            }
+
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(crawler.Crawl);
+            thread.Start();
+            CrawlerInfo.Text = "爬虫已启动....";
+        }
+    }
+}
---
+++ Homework10/ParallelCrawler/ParallelCrawler.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ParallelCrawler
+{
+    class ParallelCrawler
+    {
+        public event Action<ParallelCrawler> CrawlerStopped;
+        public event Action<ParallelCrawler, string> CrawlerInfo;
+        public event Action<ParallelCrawler, int, string, string> CrawlerDownloaded;
+
+        public string StartUrl { get; set; } //起始URL
+        public int MaxNum { get; set; } //最大数量
+
+        public static readonly string urlDetectRegex = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";   //URL检测表达式，在HTML文本中查找URL
+        public static readonly string urlParseRegex = @"^(?<site>https?://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";   //URL解析表达式
+
+        //public Hashtable urls = new Hashtable();
+
+		private readonly ConcurrentDictionary<string, bool> urls = new ConcurrentDictionary<string, bool>();
+
+		private ConcurrentQueue<string> waitingQueue = new ConcurrentQueue<string>();
+
+		public Encoding HtmlEncoding = Encoding.UTF8;
+
+        private int count = 0;
+
+		public void Crawl()
+		{
+			urls.Clear();
+			//urls.Add(StartUrl, false);
+			waitingQueue = new ConcurrentQueue<string>();
+			waitingQueue.Enqueue(StartUrl);
+
+			List<Task> tasks = new List<Task>();
+
+			/*
+			 while (count < MaxNum)
+			{
+				string current = null;
+				foreach (string url in urls.Keys)   //找到一个还没有下载过的链接
+				{
+					if ((bool)urls[url]) continue;   //已经下载过的,不再下载
+					current = url;
+				}
+				if (current == null) break;
+				CrawlerInfo(this, current);
+				try
+				{
+					string html = DownLoad(current);   //下载
+					urls[current] = true;
+					CrawlerDownloaded(this, current, "success");
+					count++;
+					Parse(html, current);   //解析,并加入新的链接
+				}
+				catch (Exception ex)
+				{
+					CrawlerDownloaded(this, current, "  Error:" + ex.Message);
+				}
+			 }
+			 */
+
+			while (tasks.Count < MaxNum)
+			{
+				if (waitingQueue.TryDequeue(out string url))
+				{
+					int index = tasks.Count;
+					Task task = Task.Run(() => DownAndParse(url, index));
+					tasks.Add(task);
+					
+				}
+			}
+
+			Task.WaitAll(tasks.ToArray());
+			CrawlerStopped(this);
+		}
+
+		private void DownAndParse(string url, int index)
+        {
+			CrawlerInfo(this, url);
+			try
+			{
+				string html = DownLoad(url, index);
+				urls[url] = true;
+				Parse(html, url);
+				CrawlerDownloaded(this, index, url, "success");
+				count++;
+			}
+			catch (Exception ex)
+			{
+				CrawlerDownloaded(this, index, url, "Error:" + ex.Message);
+			}
+		}
+
+		public string DownLoad(string url, int index)
+		{
+			WebClient webClient = new WebClient();
+			webClient.Encoding = HtmlEncoding;
+			string html = webClient.DownloadString(url);
+			File.WriteAllText(index + ".html", html, Encoding.UTF8);
+			return html;
+		}
+
+		public void Parse(string html, string pageUrl)
+		{
+			MatchCollection matches = new Regex(urlDetectRegex).Matches(html);
+
+			foreach (Match match in matches)
+			{
+				string linkUrl = match.Groups["url"].Value;
+				if (linkUrl == null || linkUrl == "" || linkUrl.StartsWith("javascript:")) continue;
+				linkUrl = FixUrl(linkUrl, pageUrl);
+
+				if (!urls.ContainsKey(linkUrl))
+				{
+					waitingQueue.Enqueue(linkUrl);
+					urls.TryAdd(linkUrl, false);
+				}
+			}
+		}
+
+		//转换为完整路径
+		static private string FixUrl(string url, string pageUrl)
+		{
+			if (url.Contains("://"))
+			{
+				return url;
+			}
+			if (url.StartsWith("//"))
+			{
+				return "http:" + url;
+			}
+			if (url.StartsWith("/"))
+			{
+				Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+				String site = urlMatch.Groups["site"].Value;
+				return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+			}
+			if (url.StartsWith("../"))
+			{
+				url = url.Substring(3);
+				int idx = pageUrl.LastIndexOf('/');
+				return FixUrl(url, pageUrl.Substring(0, idx));
+			}
+			if (url.StartsWith("./"))
+			{
+				return FixUrl(url.Substring(2), pageUrl);
+			}
+
+			int end = pageUrl.LastIndexOf("/");
+			return pageUrl.Substring(0, end) + "/" + url;
+		}
+
+	}
+}
